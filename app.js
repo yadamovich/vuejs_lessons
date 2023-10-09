5ba65b2214f9a7d0883fe151b19d72e7fd74c982
Yuri Adamovich <yuri@bloomnation.com>
2023-10-09 16:16:41 +0300

    Lesson 6 init commit

---
+++ app.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: "#app",
+    data: {
+        counter: 0
+    },
+    methods: {
+        substract: function(e) {
+            e.stopPropagation();
+            this.counter--;
+        }
+    }
+})
