2f4b82978a98cddcfc02661505a7aeaa3789622b
Yuri Adamovich <yuri@bloomnation.com>
2023-10-09 12:00:39 +0300

    Lesson 4 init commit

---
+++ app.js
@@ -0,0 +1,31 @@
+new Vue({
+    el: "#app",
+    data: {
+        people: [
+            {
+                name: 'Franky Four Fingers',
+                stillAlive: false
+            },
+            {
+                name: 'Bullet-Tooth Tony',
+                stillAlive: false
+            },
+            {
+                name: 'Turkish',
+                stillAlive: true
+            },
+            {
+                name: 'Boris the Blade',
+                stillAlive: false
+            },
+            {
+                name: 'Mickey',
+                stillAlive: true
+            },
+            {
+                name: 'Brick Top',
+                stillAlive: false
+            }
+        ]
+    }
+})
