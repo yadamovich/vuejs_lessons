061bd6249688750ecf30147944620b9298bcf937
Yuri Adamovich <yuri@bloomnation.com>
2023-10-10 17:14:27 +0300

    Lesson 7 init commit

---
+++ app.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: "#app",
+    data: {
+
+    },
+    config: {
+        keyCodes: {
+            f2: 113
+        }
+    },
+    methods: {
+        changeName: function() {
+            console.log('Name is changed!');
+        }
+    }
+})
