072369162e676c346b4146fd585c58f4c2a6879c
Yuri Adamovich <yuri@bloomnation.com>
2023-10-10 17:53:19 +0300

    Lesson 9 init commit

---
+++ app.js
@@ -0,0 +1,21 @@
+new Vue({
+    el: "#app",
+    data: {
+        a: 0,
+        b: 0,
+        age: 20
+    },
+    methods: {
+
+    },
+    computed: {
+        addToA: function() {
+            console.log('addToA');
+            return this.a + this.age;
+        },
+        addToB: function() {
+            console.log('addToB');
+            return this.b + this.age;
+        }
+    }
+})
