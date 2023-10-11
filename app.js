8563a28d1c07b6e6a6467782cbc581075c283817
Yuri Adamovich <yuri@bloomnation.com>
2023-10-11 12:50:36 +0300

    Lesson 14 init commit

---
+++ app.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: "#app-one",
+    data: {
+        title: 'Title'
+    },
+    computed: {
+
+    },
+    methods: {
+        changeTitle: function() {
+            //this.title = this.$refs.input.value;
+            console.log(this.$refs);
+        }
+    }
+});
