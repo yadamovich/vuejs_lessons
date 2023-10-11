02d26d6c8c0af7d625dcf9366038b59ebc23bb41
Yuri Adamovich <yuri@bloomnation.com>
2023-10-11 11:17:31 +0300

    Lesson 12 init commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var one = new Vue({
+    el: "#app-one",
+    data: {
+        title: 'Title one'
+    },
+    computed: {
+        greet: function() {
+            return 'Hello from Vue one!'
+        }
+    },
+    methods: {
+
+    }
+});
+
+var two = new Vue({
+    el: "#app-two",
+    data: {
+        title: 'Title two'
+    },
+    computed: {
+        greet: function() {
+            return 'Hello from Vue two!'
+        }
+    },
+    methods: {
+        changeTitleOne: function() {
+            one.title = 'Changed from Vue two!'
+        }
+    }
+});
+
+two.title = "Changed from outside!"
