3513c448cbdf817b4d8a8c55d986822072eb6be6
Yuri Adamovich <yuri@bloomnation.com>
2023-10-11 10:52:29 +0300

    Lesson 11 init commit

---
+++ app.js
@@ -0,0 +1,63 @@
+new Vue({
+    el: "#app",
+    data: {
+        currentTask: '',
+        editValue: '',
+        tasks: [
+            {
+                text: 'Subscribe to channel',
+                isCompleted: false,
+                isEditing: false
+            },
+            {
+                text: 'Like the video',
+                isCompleted: false,
+                isEditing: false
+            },
+            {
+                text: 'Learn Vue.js',
+                isCompleted: true,
+                isEditing: false
+            }
+        ]
+    },
+    methods: {
+        addTask: function() {
+            this.tasks.push({
+                text: this.currentTask,
+                isCompleted: false,
+                isEditing: false
+            });
+            this.currentTask = '';
+        },
+        removeTask: function(taskText) {
+            this.tasks = this.tasks.filter(task => {
+                return task.text !== taskText;
+            })
+        },
+        changeEditing: function(taskText) {
+            let isActiveEditing = this.tasks.filter(task => {
+                return task.isEditing;
+            });
+            if (isActiveEditing.length > 0) {
+                return false;
+            }
+            this.editValue = taskText;
+            this.tasks = this.tasks.map(task => {
+                if(task.text === taskText) {
+                    task.isEditing = !task.isEditing;
+                }
+                return task;
+            })
+        },
+        editTask: function(taskText) {
+            this.tasks = this.tasks.map(task => {
+                if(task.text === taskText) {
+                    task.isEditing = !task.isEditing;
+                    task.text = this.editValue;
+                }
+                return task;
+            })
+        }
+    }
+})
