2eb14706381cd92e0f63697d2a22ae282eb62fc9
Yuri Adamovich <yuri@bloomnation.com>
2023-10-11 11:23:40 +0300

    Lesson 13 init commit

---
+++ app.js
@@ -0,0 +1,26 @@
+Vue.component('greet', {
+    template: '<h2>{{title}} <button @click="changeTitle">Change title</button></h2>',
+    data: function() {
+        return {
+            title: 'Title template'
+        }
+    },
+    methods: {
+        changeTitle: function() {
+            this.title = 'Updated title!';
+        }
+    }
+});
+
+new Vue({
+    el: "#app-one",
+    data: {
+
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+});
